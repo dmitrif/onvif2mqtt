d40f0f19166f1c92e4531f6f0cbbc98563cb7ba1
Dmitri Farkov <dfarkov@pagerduty.com>
2020-03-27 00:37:38 -0400

    Add schema validation / config

--- src/Config.js
+++ src/Config.js
@@ -1,10 +1,12 @@
 import at from 'lodash.at';
 import path from 'path';
-import fs from 'fs';
+import fs, { exists } from 'fs';
 import yaml from 'yaml';
 import process from 'process';
+import validator from './ConfigValidator';
 import logger, { setLoggingLevel } from './Logger';
 
+const DEFAULT_CONFIG_PATH = path.resolve(__dirname, '../default-config.yml');
 const CONFIG_PATH = process.env.CONFIG_PATH || '/config/config.yml';
 
 class Config {
@@ -24,21 +26,55 @@ class Config {
     return Config.instance;
   }
 
-  _loadConfig = () => {
-    logger.info('Loading configuration.', { configPath: CONFIG_PATH });
+  _loadDefaultConfig = () => {
+    const defaultConfigFileRef = fs.readFileSync(DEFAULT_CONFIG_PATH, 'utf8');
+    return yaml.parse(defaultConfigFileRef);
+  };
 
-    const defaultConfigPath = path.resolve(__dirname, '../default-config.yml');
-    const defaultConfigFileRef = fs.readFileSync(defaultConfigPath, 'utf8');
+  _loadUserConfig = () => {
+    logger.info('Loading configuration.', { configPath: CONFIG_PATH });
 
     const configFileRef = fs.readFileSync(CONFIG_PATH, 'utf8');
 
-    const parsedDefaultConfig = yaml.parse(defaultConfigFileRef);
-    const parsedConfig = yaml.parse(configFileRef);
+    let parsedConfig = {};
 
-    return {
-      ...parsedDefaultConfig,
-      ...parsedConfig
+    try {
+      parsedConfig = yaml.parse(configFileRef);
+    } catch(e) {
+      logger.error('Invalid YAML found in configuration', { source: e.source });
+      logger.error(e);
+      logger.error();
+      process.exit(1);
+    }
+
+    return parsedConfig;
+  };
+
+  _validate = (config) => {
+    logger.info('Validating configuration file.');
+    const validationErrors = validator.validate(config);
+
+    if (validationErrors.length > 0) {
+      logger.error('Config validation failed...');
+      validationErrors.forEach(({ path, message }) => {
+        logger.error(message, { path });
+      });
+      process.exit(1);
+    }
+  };
+
+  _loadConfig = () => {
+    const defaultConfig = this._loadDefaultConfig();
+    const userConfig = this._loadUserConfig();
+
+    const mergedConfig = {
+      ...defaultConfig,
+      ...userConfig
     };
+
+    this._validate(mergedConfig);
+
+    return mergedConfig;
   };
 
   get = path => at(this._config, path)[0];
