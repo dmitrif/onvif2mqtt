ecb45c88a2b406529605c45a29c52530a1a795a7
Michael Bogatyrev <ksupipr@yandex.ru>
2021-01-19 17:20:21 +0500

    simpleItem array to object

--- src/Manager.js
+++ src/Manager.js
@@ -74,8 +74,9 @@ export default class Manager {
   };
 
   /* Event Callbacks */
-  onMotionDetected = debounceStateUpdate((onvifDeviceId, boolMotionState) => {
+  onMotionDetected = debounceStateUpdate((onvifDeviceId, motionState) => {
     const topicKey = 'motion';
+    const boolMotionState = motionState.IsMotion;
 
     this.publishTemplates(onvifDeviceId, topicKey, boolMotionState);
     this.publisher.publish(onvifDeviceId, topicKey, convertBooleanToSensorState(boolMotionState));
